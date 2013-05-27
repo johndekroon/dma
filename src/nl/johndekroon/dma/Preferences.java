9aeab504a7141f3c998f75a5c7dbbc20be2d5a5f
John de Kroon <johndekroon@gmail.com>
2013-05-27 23:53:26 +0200

    preferences fix

--- src/nl/johndekroon/dma/Preferences.java
+++ src/nl/johndekroon/dma/Preferences.java
@@ -14,6 +14,7 @@ public class Preferences extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
 	}
 
 	@Override
